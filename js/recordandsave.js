a1bf3f4d3f6016fe8353ef8f7aa1efb3eeda161f
psharmz <s1466415@sms.ed.ac.uk>
2017-04-29 13:29:37 +0100

    added recording functionality for users

--- js/recordandsave.js
+++ js/recordandsave.js
@@ -1,11 +1,154 @@
-function uniKeyCode(event) {
-    var key = event.keyCode;
-    //go back to record home
-    if (key == 55) {
-        window.location = 'recordhome.html';
-    }
-    //continue onto the next page
-    if (key == 57) {
-        window.location = 'thanks.html';
+if ( navigator.mediaDevices === undefined ) {
+    navigator.mediaDevices = {};
+}
+
+
+if ( navigator.mediaDevices.getUserMedia === undefined ) {
+    navigator.mediaDevices.getUserMedia = function ( constrains ) {
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+        if ( !getUserMedia )  {
+            return Promise.reject( new Error( 'getUserMedia is not implemented in this browser' ) );
+        }
+
+        return new Promise( function( resolve, reject ) {
+            getUserMedia.call( navigator, constrains, resolve, reject );
+        } );
     }
 }
+
+if ( navigator.mediaDevices.getUserMedia ) {
+    var recording_status = 'inactive',
+        mediaRecorder,
+        chunks = [],
+        audio = new Audio(),
+        mediaStream,
+        audioSrc,
+        type = {
+            'type': 'audio/ogg,codecs=opus'
+        },
+        ctx,
+        analys,
+        blob;
+
+    // set the key functions
+    function uniKeyCode(event) {
+        var key = event.keyCode;
+        //go back to record home
+        if (key == 55) {
+            window.location = 'recordhome.html';
+        }
+        //continue onto the next page
+        if (key == 57) {
+            window.location = 'thanks.html';
+        }
+        //begin recording on key press 1 
+        if (key == 49) {
+            if ( recording_status == 'inactive' ) {
+                start();
+            } 
+        }
+
+        //stop recording on key press 4
+        if (key == 52) {
+            if ( recording_status == 'recording' ) {
+                stop();
+            }      
+        }
+
+        //replay recording on key press 3
+        if (key == 51) {
+            play();   
+        }
+
+        //submit recording on key press 9 
+        if (key == 57) {
+            save();
+        }       
+
+    }
+
+    // start recording function
+    function start() {
+        navigator.mediaDevices.getUserMedia( { 'audio': true } ).then( function ( stream ) {
+            mediaRecorder = new MediaRecorder( stream );
+            mediaRecorder.start();
+
+            // button.classList.add( 'recording' );
+            recording_status = 'recording';
+
+          
+            if ( navigator.vibrate ) navigator.vibrate( 150 );
+
+            time = Math.ceil( new Date().getTime() / 1000 );
+
+
+            mediaRecorder.ondataavailable = function ( event ) {
+                chunks.push( event.data );
+            }
+
+            mediaRecorder.onstop = function () {
+                stream.getTracks().forEach( function( track ) { track.stop() } );
+
+                blob = new Blob( chunks, type );
+                audioSrc = window.URL.createObjectURL( blob );
+
+                audio.src = audioSrc;
+
+                chunks = [];
+            }   
+
+            
+            
+        } ).catch( function ( error ) {
+            if ( location.protocol != 'https:' ) {
+              msg_box.innerHTML = lang.mic_error + '<br>'  + lang.use_https;
+            } else {
+              msg_box.innerHTML = lang.mic_error; 
+            }
+            button.disabled = true;
+        });
+    }
+
+    // stop recording function
+    function stop() {
+        mediaRecorder.stop();
+        // button.classList.remove( 'recording' );
+        recording_status = 'inactive';
+      
+        if ( navigator.vibrate ) navigator.vibrate( [ 200, 100, 200 ] );
+
+        var now = Math.ceil( new Date().getTime() / 1000 );
+
+        var t = parseTime( now - time );
+
+        msg_box.innerHTML = '<a href="#" onclick="play(); return false;" class="txt_btn">' + lang.play + ' (' + t + 's)</a><br>' +
+                            '<a href="#" onclick="save(); return false;" class="txt_btn">' + lang.download + '</a>'
+    }
+
+    //play back the recording
+    function play() {
+        audio.play();
+    }
+
+    //save the recording
+    function save() {
+        var a = document.createElement( 'a' );
+        a.download = 'record.ogg';
+        a.href = audioSrc;
+        document.body.appendChild( a );
+        a.click();
+
+        document.body.removeChild( a );
+    }
+
+} else {
+    if ( location.protocol != 'https:' ) {
+      msg_box.innerHTML = lang.mic_error + '<br>'  + lang.use_https;
+    } else {
+      msg_box.innerHTML = lang.mic_error; 
+    }
+    // button.disabled = true;
+}
+
+
+
